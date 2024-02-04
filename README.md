b1c3232dd557be8ea37002552655e72b6fe9d0a9
Luis Augusto <blackbarth@outlook.com>
2024-02-04 18:50:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dio
+ Projeto Flutter DIO.ME
---
+++ android/app/src/main/kotlin/br/com/luisaugustoferreira/dio_1/dio/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.luisaugustoferreira.dio_1.dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
